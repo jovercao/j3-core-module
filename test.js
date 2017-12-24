cbace4e7c7ea621a59231ec5b55cb85c800b86de
Jover Cao <jovercao@live.com>
2017-12-24 23:54:14 +0800

    123312

--- test.js
+++ test.js
@@ -1,66 +1,26 @@
-// /*
-// const fs = require('fs')
+import fs from 'fs-promise'
 
-// // fs.readFile('d:\\a.txt')
-// //     .then((ret) => {
-// //         console.log(ret)
-// //     })
-// //     .error((err) => {
-// //         console.log(err)
-// //     })
+async function doReadFile() {
+    try {
+        let data = await fs.readFile('d:\\a.txt')
+        console.log(data)
+    } catch (err) {
+        console.error(err)
+    }
+}
 
-
-
-// function readFile() {
-//     return new Promise(function (resolve, reject) {
-//         fs.readFile('d:\\a.txt', (err, data) => {
-//             if (err) {
-//                 reject(err)
-//             } else {
-//                 resolve(data)
-//             }
-//         })
-//     })
-// }
-
-
-// async function doReadFile() {
-//     try {
-//         let data = await readFile()
-//         console.log(data)
-//     } catch (err) {
-//         console.error(err)
-//     }
-// }
-
-
-// */
-
-// // import fs from 'fs-promise'
-
-// // async function doReadFile() {
-// //     try {
-// //         let data = await fs.readFile('d:\\a.txt')
-// //         console.log(data)
-// //     } catch (err) {
-// //         console.error(err)
-// //     }
-// // }
-
-// // doReadFile()
+doReadFile()
 
 
 
-import { download } from './src'
+// import { download } from './src'
 
-var download = require('./src')
+// download('node-sql')
 
-//download('node-sql')
-
-npmInstall('', info => console.log(info), err => console.error(err)).then(() => {
-    console.log(i18n.__('install_complete'))
-})
-.error(err => {
-    console.error(i18n.__('install_failure'))
-    console.log(err)
-})
+// npmInstall('', info => console.log(info), err => console.error(err)).then(() => {
+//     console.log(i18n.__('install_complete'))
+// })
+// .error(err => {
+//     console.error(i18n.__('install_failure'))
+//     console.log(err)
+// })
